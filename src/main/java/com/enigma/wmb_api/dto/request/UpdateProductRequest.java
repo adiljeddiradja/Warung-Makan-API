c9cdd9d8bc760b3c6a781a3bfe8be6d7025bc529
Adil Jeddi Radja <radjaajr@gmail.com>
2024-03-09 16:05:32 +0700

    [Add] add method find,update and delete in MenuServiceImpl

--- src/main/java/com/enigma/wmb_api/dto/request/UpdateProductRequest.java
+++ src/main/java/com/enigma/wmb_api/dto/request/UpdateProductRequest.java
@@ -1,5 +1,12 @@
 package com.enigma.wmb_api.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UpdateProductRequest {
     private String id;
     private String name;
--- src/main/java/com/enigma/wmb_api/service/impl/MenuServiceImpl.java
+++ src/main/java/com/enigma/wmb_api/service/impl/MenuServiceImpl.java
@@ -8,11 +8,17 @@ import com.enigma.wmb_api.dto.response.MenuRespone;
 import com.enigma.wmb_api.entity.Menu;
 import com.enigma.wmb_api.repository.MenuRepository;
 import com.enigma.wmb_api.service.MenuService;
+import com.enigma.wmb_api.spesification.MenuSpesification;
 import com.enigma.wmb_api.util.ValidationUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -21,7 +27,7 @@ public class MenuServiceImpl implements MenuService {
     private final MenuRepository menuRepository;
     private final ValidationUtil validationUtil;
 
-
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public MenuRespone create(NewMenuRequest request) {
         validationUtil.validate(request);
@@ -36,33 +42,49 @@ public class MenuServiceImpl implements MenuService {
 
     @Override
     public MenuRespone getOneById(String id) {
-        return null;
+        Menu menu = findByIdOrThrowNotFound(id);
+        return convertMenuToMenuResponse(menu);
     }
 
     @Override
     public Menu getById(String id) {
-        return null;
-    }
+        return findByIdOrThrowNotFound(id);
 
+    }
+    @Transactional(readOnly = true)
     @Override
     public Page<MenuRespone> getAll(SearchMenuRequest request) {
-        return null;
+        if (request.getPage() <= 0) request.setPage(1);
+
+        Sort sort = Sort.by(Sort.Direction.fromString(request.getDirection()), request.getSortBy());
+        Pageable pageable = PageRequest.of((request.getPage() - 1), request.getSize(), sort);
+        Specification<Menu> specification = MenuSpesification.getSpecification(request);
+        return menuRepository.findAll(specification, pageable).map(this::convertMenuToMenuResponse);
     }
 
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public MenuRespone update(UpdateProductRequest request) {
-        return null;
+        Menu currentMenu = findByIdOrThrowNotFound(request.getId());
+        currentMenu.setName(request.getName());
+        currentMenu.setPrice(currentMenu.getPrice());
+        currentMenu.setStock(currentMenu.getStock());
+        menuRepository.saveAndFlush(currentMenu);
+        return convertMenuToMenuResponse(currentMenu);
     }
 
     @Override
     public Menu update(Menu menu) {
-        return null;
+        return menuRepository.saveAndFlush(menu);
     }
-
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void deleteById(String id) {
+        Menu currentMenu = getById(id);
+        menuRepository.delete(currentMenu);
 
     }
+
     private MenuRespone convertMenuToMenuResponse(Menu product) {
         return MenuRespone.builder()
                 .id(product.getId())
@@ -71,6 +93,7 @@ public class MenuServiceImpl implements MenuService {
                 .stock(product.getStock())
                 .build();
     }
+
     private Menu findByIdOrThrowNotFound(String id) {
         return menuRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, ResponseMessage.ERROR_NOT_FOUND));
     }
