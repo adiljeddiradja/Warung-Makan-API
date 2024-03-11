deadac2c5e33bf87e37c01b70ed50d9628a6e5d2
Adil Jeddi Radja <radjaajr@gmail.com>
2024-03-12 01:09:07 +0700

    [FEATURE] ADD SECURTY,AUTHENTICATION, AND AUTHORIZATION(CREATE SERVICE,SECURITY,JWT TOKEN HANDLER,CONTROLLER)

---
+++ src/main/java/com/enigma/wmb_api/repository/UserAccountRepository.java
@@ -0,0 +1,14 @@
+package com.enigma.wmb_api.repository;
+
+;
+import com.enigma.wmb_api.entity.UserAccount;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+    Optional<UserAccount> findByUsername(String username);
+}
